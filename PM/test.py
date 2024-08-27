5dd541d692a8343237e54071ebec40d6c466ad3b
Alexander Pollak <Alexander.Pollak.87@gmail.com>
2024-08-27 21:35:07 +0200

    test

--- PM/test.py
+++ PM/test.py
@@ -1,20 +1,23 @@
 
-from pysnmp.hlapi.v3arch.asyncio import *
-from pysnmp.smi.rfc1902 import ObjectIdentity, ObjectType
+from pysnmp.hlapi import *
 
-with Slim(1) as slim:
-    errorIndication, errorStatus, errorIndex, varBinds = await
-    slim.get('public', '192.168.0.216', '161', ObjectType(ObjectIdentity("SNMPv2-MIB", "sysDescr", 0)), )
+iterator = getCmd(
+    SnmpEngine(),
+    CommunityData('public', mpModel=0),
+    UdpTransportTarget(('192.168.0.216', 161)),
+    ContextData(),
+    ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0))
+)
 
-    if errorIndication:
-        print(errorIndication)
-    elif errorStatus:
-        print(
-            "{} at {}".format(
-                errorStatus.prettyPrint(),
-                errorIndex and varBinds[int(errorIndex) - 1][0] or "?",
-            )
-        )
-    else:
-        for varBind in varBinds:
-            print(" = ".join([x.prettyPrint() for x in varBind]))
+errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
+
+if errorIndication:
+    print(errorIndication)
+
+elif errorStatus:
+    print('%s at %s' % (errorStatus.prettyPrint(),
+                        errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+
+else:
+    for varBind in varBinds:
+        print(' = '.join([x.prettyPrint() for x in varBind]))
