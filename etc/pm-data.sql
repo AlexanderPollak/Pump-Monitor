e5920d05d9505ddbb64b214ea78637472091917d
Alexander Pollak <Alexander.Pollak.87@gmail.com>
2024-10-09 11:10:31 +0200

    bug fix

--- etc/pm-data.sql
+++ etc/pm-data.sql
@@ -18,10 +18,10 @@
 
 
 --
--- Table structure for table `pumpdata`
+-- Table structure for table `pump_data`
 --
 
-DROP TABLE IF EXISTS `pumpdata`;
+DROP TABLE IF EXISTS `pump_data`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pump_data` (
--- lib/mysql_write.py
+++ lib/mysql_write.py
@@ -36,7 +36,7 @@ class MySQL_com():
         if self._port !=0:
             self.close()
 
-    def open (self,HOST='localhost',USER ='grafanauser',PASSWORD='Mars2020',DATABASE='scpdata',AUTH_PLUGIN='mysql_native_password'):
+    def open (self,HOST='localhost',USER ='grafanauser',PASSWORD='Mars2020',DATABASE='pumpdata',AUTH_PLUGIN='mysql_native_password'):
         """Establishing the connection to the mqsql database
 
         Args:
@@ -84,7 +84,7 @@ class MySQL_com():
             [Water Level, System Temperature, Pump 1 Status, Pump 2 Status, Pump 3 Status, System Fault] dtype=float64 and dtype=str.
 
 
-        DROP TABLE IF EXISTS `pumpdata`;
+        DROP TABLE IF EXISTS `pump_data`;
         CREATE TABLE `pump_data` (
             `ts` datetime NOT NULL,
             `water_level` float DEFAULT (NULL),
@@ -108,7 +108,7 @@ class MySQL_com():
         tmp_PS = PS_LIST
 
         # Preparing SQL query to INSERT a record into the database.
-        tmp_sql = "INSERT INTO pumpdata (ts,water_level,temperature,pump1_status,pump2_status,pump3_status,system_status) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+        tmp_sql = "INSERT INTO pump_data (ts,water_level,temperature,pump1_status,pump2_status,pump3_status,system_status) VALUES (%s,%s,%s,%s,%s,%s,%s)"
         tmp_val = [(tmp_time, tmp_PS[0][0], tmp_PS[0][1], tmp_PS[0][2], tmp_PS[0][3], tmp_PS[0][4], tmp_PS[0][5])]
 
         try:
